ddef33aeb0acb7798ad53b2318460740a387a63a
molsbee <molsbee@gmail.com>
2022-08-20 15:26:07 -0500

    Enabled go mod for dependencies

---
+++ go.mod
@@ -0,0 +1,16 @@
+module github.com/Molsbee/crypt
+
+go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.8.1 // indirect
+	github.com/go-playground/validator/v10 v10.11.0 // indirect
+	github.com/goccy/go-json v0.9.11 // indirect
+	github.com/mattn/go-isatty v0.0.16 // indirect
+	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
+	github.com/pelletier/go-toml/v2 v2.0.3 // indirect
+	golang.org/x/crypto v0.0.0-20220817201139-bc19a97f63c8 // indirect
+	golang.org/x/net v0.0.0-20220812174116-3211cb980234 // indirect
+	golang.org/x/sys v0.0.0-20220818161305-2296e01440c6 // indirect
+	google.golang.org/protobuf v1.28.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-	"crypto/aes"
-	"crypto/cipher"
-	"crypto/rand"
-	"io"
-	"os"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
-	// Encrypt File
-	data, _ := os.ReadFile("temp.txt")
+	// Client should encrypt/decrypt
+	// Server should store/retrieve encrypted data associated with user
+	router := gin.Default()
+	router.Run()
 
-	block, _ := aes.NewCipher([]byte("TestingSomething"))
-	gcm, _ := cipher.NewGCM(block)
-	nonce := make([]byte, gcm.NonceSize())
-	io.ReadFull(rand.Reader, nonce)
-	ciphertext := gcm.Seal(nonce, nonce, data, nil)
-	os.WriteFile("ciphertext.bin", ciphertext, 0777)
+	// Encrypt File
+	//data, _ := os.ReadFile("temp.txt")
+
+	//block, _ := aes.NewCipher([]byte("TestingSomething"))
+	//gcm, _ := cipher.NewGCM(block)
+	//nonce := make([]byte, gcm.NonceSize())
+	//io.ReadFull(rand.Reader, nonce)
+	//ciphertext := gcm.Seal(nonce, nonce, data, nil)
+	//os.WriteFile("ciphertext.bin", ciphertext, 0777)
 
 	// Decrypt Encrypted File
-	ciphertext, _ = os.ReadFile("ciphertext.bin")
-	nonce = ciphertext[:gcm.NonceSize()]
-	ciphertext = ciphertext[gcm.NonceSize():]
-	plaintext, _ := gcm.Open(nil, nonce, ciphertext, nil)
-	os.WriteFile("plaintext.txt", plaintext, 0777)
+	//ciphertext, _ = os.ReadFile("ciphertext.bin")
+	//nonce = ciphertext[:gcm.NonceSize()]
+	//ciphertext = ciphertext[gcm.NonceSize():]
+	//plaintext, _ := gcm.Open(nil, nonce, ciphertext, nil)
+	//os.WriteFile("plaintext.txt", plaintext, 0777)
 }
