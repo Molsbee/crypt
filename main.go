dbbd3d6ad55ab6f8e63c3ad194ef322fc0142673
molsbee <molsbee@gmail.com>
2022-08-27 11:44:09 -0500

    Playing around with fyne

--- main.go
+++ main.go
@@ -3,19 +3,39 @@ package main
 import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/container"
 	"fyne.io/fyne/v2/driver/desktop"
 	"fyne.io/fyne/v2/widget"
 	"github.com/Molsbee/crypt/assets"
 	"log"
+	"os"
 )
+
+var mainWindow fyne.Window
 
 func main() {
 	a := app.New()
 	a.SetIcon(assets.Logo)
 	setupTray(a)
-	w := a.NewWindow("Hello World")
-	w.SetContent(widget.NewLabel("Hello World!"))
-	w.ShowAndRun()
+	mainWindow = a.NewWindow("Crypt")
+	mainWindow.SetContent(loginPage(func() {
+		mainWindow.SetContent(mainApp())
+	}))
+	mainWindow.Resize(fyne.NewSize(640, 460))
+
+	//mainWindow.SetContent(split)
+	//tabs := container.NewAppTabs(
+	//	container.NewTabItem("Tab 1", widget.NewLabel("Tab 1")),
+	//	container.NewTabItem("Tab 2", widget.NewLabel("Tab 2")),
+	//)
+	//tabs.SetTabLocation(container.TabLocationLeading)
+	//mainWindow.SetContent(tabs)
+	mainWindow.SetOnClosed(func() {
+		os.Exit(0)
+	})
+
+	mainWindow.ShowAndRun()
 }
 
 func setupTray(a fyne.App) {
@@ -29,4 +49,37 @@ func setupTray(a fyne.App) {
 		}
 		desk.SetSystemTrayMenu(menu)
 	}
+}
+
+func loginPage(onsubmit func()) *fyne.Container {
+	username := widget.NewEntry()
+	passphrase := widget.NewPasswordEntry()
+
+	form := widget.NewForm(
+		widget.NewFormItem("Username", username),
+		widget.NewFormItem("Passphrase", passphrase),
+	)
+	form.OnSubmit = onsubmit
+	return container.NewPadded(form)
+}
+
+func mainApp() *container.Split {
+	data := container.NewMax()
+
+	menu := container.NewVBox(
+		widget.NewButton("Images", func() {
+			img := canvas.NewImageFromResource(assets.Logo)
+			data.RemoveAll()
+			data.Add(container.NewGridWrap(fyne.NewSize(50, 50), img, img, img, img, img))
+			//data = container.NewGridWrap(fyne.NewSize(50, 50), img, img, img, img, img)
+			//data.Refresh()
+			log.Println("Tab 1")
+		}),
+		widget.NewButton("Tab 2", func() {
+			log.Println("Tab 2")
+		}),
+	)
+	split := container.NewHSplit(menu, data)
+	split.Offset = 0.2
+	return split
 }
